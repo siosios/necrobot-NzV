ec1de81c25de59ec447ca7af985cfd5f39964814
Sébastien [NzV] <donm7v@gmail.com>
2016-09-07 21:51:43 +0200

    ...

--- PoGo.NecroBot.GUI/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.GUI/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using Awesomium.Core;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion(ReleaseInfo.ASSEMBLY_VERSION)]
-[assembly: AssemblyFileVersion(ReleaseInfo.ASSEMBLY_VERSION)]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
